98045b087ea5eba9606dd8b4f0b3d3fd2df05a51
Vishnu Pillai (ADE) <vpillai@ade.arkgov.net>
2017-08-29 13:49:13 -0500

    added gitignore for bin folder

--- OwnerClaim.sol
+++ OwnerClaim.sol
@@ -3,9 +3,12 @@ pragma solidity ^0.4.2;
 contract OwnerClaims {
 
     string constant public DEFAULT_KEY = "default";
-
+    
+    //this serves as a 2D key value pair
     mapping(address => mapping(string => string)) private owners;
 
+    //the owner of the contract can claim values for keys, e.g., FirstName = "Vishnu"
+    // "LastName"=Pillai etc. 
     function setClaim(string key, string value) {
         owners[msg.sender][key] = value;
     }
