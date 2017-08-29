25427c7072354891d600e77a6e0a4a66886bbd83
Vishnu Pillai (ADE) <vpillai@ade.arkgov.net>
2017-08-29 15:30:53 -0500

    added AssetOwnership Folder

--- ClaimOwnership/OwnerClaim.sol
+++ ClaimOwnership/OwnerClaim.sol
@@ -1,18 +1,21 @@
 pragma solidity ^0.4.2;
-
+import 'CoinExample/VishCoin.sol';
+//contract OwnerClaims is VishCoin {
 contract OwnerClaims {
-
     string constant public DEFAULT_KEY = "default";
     
     //this serves as a 2D key value pair
     mapping(address => mapping(string => string)) private owners;
 
-    //the owner of the contract can claim values for keys, e.g., FirstName = "Vishnu"
-    // "LastName"=Pillai etc. 
+    uint testCoin = 100;    
+    
+    //the owner of the contract can claim values for keys, e.g., FirstName = "John"
+    // "LastName"=Doe etc. 
     function setClaim(string key, string value) {
         owners[msg.sender][key] = value;
+        //mint(msg.sender, testCoin);
     }
-
+   
     function getClaim(address owner, string key) constant returns (string) {
         return owners[owner][key];
     }
